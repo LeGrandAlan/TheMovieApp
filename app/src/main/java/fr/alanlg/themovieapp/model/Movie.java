6051d9fecdb9ddf04d3bf778b683235dd086dc1b
alanlg <pro.alan.le.grand@gmail.com>
2019-03-19 15:30:37 +0100

    Ajout du modèle Movie

---
+++ app/src/main/java/fr/alanlg/themovieapp/model/Movie.java
@@ -0,0 +1,74 @@
+package fr.alanlg.themovieapp.model;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Movie {
+
+    private int id;
+    private String title;
+    @SerializedName("overview")
+    private String description;
+    @SerializedName("release_date")
+    private String releaseDate;
+    @SerializedName("vote_count")
+    private int voteCount;
+    @SerializedName("vote_average")
+    private float voteAverage;
+    @SerializedName("poster_path")
+    private String posterPath;
+
+    public Movie(int id, String title, String description, String releaseDate, int voteCount, float voteAverage, String posterPath) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.releaseDate = releaseDate;
+        this.voteCount = voteCount;
+        this.voteAverage = voteAverage;
+        this.posterPath = posterPath;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public int getVoteCount() {
+        return voteCount;
+    }
+
+    public float getVoteAverage() {
+        return voteAverage;
+    }
+
+    public String getPosterLink(int widthInPixel) {
+        if (this.posterPath == null) {
+            return null;
+        }
+        return "https://image.tmdb.org/t/p/w" + widthInPixel + posterPath;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", releaseDate='" + releaseDate + '\'' +
+                ", voteCount=" + voteCount +
+                ", voteAverage=" + voteAverage +
+                ", posterPath='" + this.getPosterLink(500) + '\'' +
+                '}';
+    }
+
+}
