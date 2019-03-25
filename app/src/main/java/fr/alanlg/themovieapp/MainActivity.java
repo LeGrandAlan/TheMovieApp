6f751dfb26ff13a0c24f67bfcd8b32a8cc365539
alanlg <pro.alan.le.grand@gmail.com>
2019-03-25 17:18:12 +0100

    Ajout connection/enregistrement/déconnection

--- app/src/main/java/fr/alanlg/themovieapp/MainActivity.java
+++ app/src/main/java/fr/alanlg/themovieapp/MainActivity.java
@@ -4,24 +4,36 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
-import android.view.View;
-import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.SeekBar;
-import android.widget.Spinner;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private static final int RC_SIGN_IN = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,17 +52,45 @@ public class MainActivity extends AppCompatActivity
         });
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        ActionBarDrawerToggle toggle =
+                new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-
         loadFragment(new HomeFragment());
+    }
 
+    private void refreshUserDisplay(FirebaseUser user, boolean connected) {
+        TextView userDisplayName = findViewById(R.id.userDisplayName);
+        TextView userEmail = findViewById(R.id.userEmail);
+
+        if(connected) {
+            userDisplayName.setText(user.getDisplayName());
+            userEmail.setText(user.getEmail());
+
+            //changement de l'affichage du menu
+            NavigationView navigationView = findViewById(R.id.nav_view);
+            navigationView.getMenu().findItem(R.id.nav_connection).setVisible(false);
+            navigationView.getMenu().findItem(R.id.nav_parameters).setVisible(true);
+            navigationView.getMenu().findItem(R.id.nav_disconnection).setVisible(true);
+
+            Snackbar.make(findViewById(R.id.drawer_layout), "Vous êtes connecté en tant que " + user.getDisplayName() + " !", Snackbar.LENGTH_LONG).show();
+        } else {
+            userDisplayName.setText(R.string.nav_header_title);
+            userEmail.setText(R.string.nav_header_subtitle);
+
+            //changement de l'affichage du menu
+            NavigationView navigationView = findViewById(R.id.nav_view);
+            navigationView.getMenu().findItem(R.id.nav_connection).setVisible(true);
+            navigationView.getMenu().findItem(R.id.nav_parameters).setVisible(false);
+            navigationView.getMenu().findItem(R.id.nav_disconnection).setVisible(false);
+
+            Snackbar.make(findViewById(R.id.drawer_layout), "Vous êtes bien déconnecté !", Snackbar.LENGTH_LONG).show();
+        }
 
     }
 
@@ -65,30 +105,43 @@ public class MainActivity extends AppCompatActivity
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                this.refreshUserDisplay(user, true);
+
+            } else {
+                if (response == null) {
+                    Toast.makeText(this.getApplicationContext(), "Connection annulée", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this.getApplicationContext(), "Problème de connection", Toast.LENGTH_SHORT).show();
+                    Log.d("Erreur connection : ", Objects.requireNonNull(response.getError()).getMessage());
+                }
+            }
+        }
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public boolean onCreateOptionsMenu(Menu menu) {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (user != null) {
+            refreshUserDisplay(user, true);
         }
 
-        return super.onOptionsItemSelected(item);
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        // Handle navigation view item clicks here.
+
         int id = item.getItemId();
 
         Fragment fragment = null;
@@ -99,11 +152,34 @@ public class MainActivity extends AppCompatActivity
             fragment = new SearchFragment();
         } else if (id == R.id.nav_top_rated) {
             fragment = new TopRatedFragment();
+        } else if (id == R.id.nav_connection) {
+            List<AuthUI.IdpConfig> providers = Arrays.asList(
+                    new AuthUI.IdpConfig.EmailBuilder().build(),
+                    new AuthUI.IdpConfig.GoogleBuilder().build());
+
+            startActivityForResult(
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setAvailableProviders(providers)
+                            .build(),
+                    RC_SIGN_IN);
+        } else if (id == R.id.nav_disconnection) {
+            this.signOut();
         }
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return loadFragment(fragment);
+    }
+
+    private void signOut() {
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    public void onComplete(@NonNull Task<Void> task) {
+                        refreshUserDisplay(null, false);
+                    }
+                });
     }
 
     private boolean loadFragment(Fragment fragment) {
